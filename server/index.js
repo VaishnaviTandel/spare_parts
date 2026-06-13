7a2357cb6900404c19cbfd29e9734ac8274955f0
VaishnaviTandel <vaishu.tandel0501@gmail.com>
2026-06-13 20:45:21 +0530

    add explicit CORS headers and OPTIONS handling

--- server/index.js
+++ server/index.js
@@ -28,6 +28,19 @@ const corsOptions = {
   credentials: true,
 }
 app.use(cors(corsOptions))
+app.use((req, res, next) => {
+  const origin = req.headers.origin
+  if (origin && allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin)
+    res.header('Access-Control-Allow-Credentials', 'true')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+  }
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204)
+  }
+  next()
+})
 app.options('*', cors(corsOptions))
 app.use(express.json())
 

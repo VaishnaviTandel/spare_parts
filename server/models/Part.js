515a7262b4b3a8a2ede14ed0737c9b9f5ad20e2a
VaishnaviTandel <vaishu.tandel0501@gmail.com>
2026-06-14 13:39:29 +0530

    added parts 3.5hp

--- server/models/Part.js
+++ server/models/Part.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const partSchema = new mongoose.Schema(
   {
     owner:    { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    hp:       { type: String, enum: ['2.5', '6'], required: true },
+    hp:       { type: String, enum: ['2.5', '6', '3.5'], required: true },
     category: { type: String, required: true, trim: true },
     name:     { type: String, required: true, trim: true },
     price:    { type: Number, required: true, min: 0 },
